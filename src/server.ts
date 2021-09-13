dc6377e568a97608e588ab90723ad973cbac6df9
arthur.barbero <arthur-barbero@hotmail.com>
2021-09-13 20:29:55 -0300

    fix(Docker-Compose): Adicionado a porta às variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import App from './app'
 // Getting the instance from App Class
 const server = new App().instance
 
-server.listen(3333, () => console.log('🔮 magic happens on port 3333'))
+server.listen(process.env.NODE_PORT, () => console.log(`🔮 magic happens on port ${process.env.NODE_PORT}`))
