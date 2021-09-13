c56a66e1776c5fa54f59bba2ff9c7d72ef4a8a67
arthur.barbero <arthur-barbero@hotmail.com>
2021-09-13 15:43:15 -0300

    feature(App-Server): Adcionado alguns comentários simples

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,35 @@
-import express from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
 import routes from './routes'
 import dbInt from './Entities/migration'
 import morgan from 'morgan'
 
 class App {
-  public instance: express.Application
+  public instance: Application
 
   public constructor () {
+    // Initiating a express App
     this.instance = express()
 
+    // Setting up dependencies in a basic way
     this.middlewares()
     this.database()
     this.routes()
   }
 
+  // Middlewares for express
   private middlewares (): void {
     this.instance.use(express.json())
     this.instance.use(cors())
     this.instance.use(morgan('dev'))
   }
 
+  // Middleware for initiating database on sequelize
   private database (): void {
     dbInt()
   }
 
+  // Controler's routes to reach application features
   private routes (): void {
     this.instance.use(routes)
   }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import App from './app'
 
+// Getting the instance from App Class
 const server = new App().instance
 
 server.listen(3333, () => console.log('🔮 magic happens on port 3333'))
